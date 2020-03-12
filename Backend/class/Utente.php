bf7b2e18f63169692bc99282232ac25305e03a82
Pietro Bonechi <p.bonechi1@gmail.com>
2020-03-12 15:35:21 +0100

    fix registration

--- Backend/class/Utente.php
+++ Backend/class/Utente.php
@@ -11,10 +11,10 @@ class Utente
 {
     protected $db;
     public $_id;
-    public $_name;
-    public $_surname;
-    public $_sidiCode;
-    public $_taxCode;
+    public $_nome;
+    public $_cognome;
+    public $_email;
+    public $_password;
  
     public function __construct() {
         $this->db = new DBConnection();
@@ -27,13 +27,13 @@ class Utente
 		Nella prima parte esegue l' aggiunta del nuovo studente
 		*/
 		try {
-    		$sql = 'INSERT INTO utenti (nome, cognome, email, password,sesso)  VALUES (:nome, :cognome, :email, :password, :sesso)';
+    		$sql = 'INSERT INTO utenti (nome, cognome, email, password)  VALUES (:nome, :cognome, :email, :password)';
     		$data = [
 			    'nome' => $this->_nome,
 			    'cognome' => $this->_cognome,
 			    'email' => $this->_email,
 				'password' => $this->_password,
-				'sesso' => $this->_sesso,
+
 			];
 	    	$stmt = $this->db->prepare($sql);
 	    	$stmt->execute($data);
@@ -44,132 +44,7 @@ class Utente
     		die("Oh noes! There's an error in the query!".$e);
 		}
 
-		/*
-		Nella seconda parte esegue la visualizzazione del nuovo studente
-		*/
-		try {
-    		$sql = "SELECT * FROM student WHERE sidi_Code=:sidiCode";
-		    $stmt = $this->db->prepare($sql);
-		    $data = [
-		    	'sidiCode' => $this->_sidiCode
-			];
-		    $stmt->execute($data);
-		    $result = $stmt->fetch(\PDO::FETCH_ASSOC);
-            return $result;
-		} catch (Exception $e) {
-		    die("Oh noes! There's an error in the query!");
-		}
- 
-    }
-   
-    // getAll 
-    public function list() {
-    	try {
-    		$sql = "SELECT * FROM student";
-		    $stmt = $this->db->prepare($sql);
- 
-		    $stmt->execute();
-		    $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-            return $result;
-		} catch (Exception $e) {
-		    die("Oh noes! There's an error in the query!");
-		}
-    }
-
-    // getOne
-    public function one() {
-    	try {
-    		$sql = "SELECT * FROM student WHERE id=:id";
-		    $stmt = $this->db->prepare($sql);
-		    $data = [
-		    	'id' => $this->_id
-			];
-		    $stmt->execute($data);
-		    $result = $stmt->fetch(\PDO::FETCH_ASSOC);
-            return $result;
-		} catch (Exception $e) {
-		    die("Oh noes! There's an error in the query!");
-		}
-    }
- 
-    // delete TODO
-    public function delete() {
-		try {
-    		$sql = "DELETE FROM student WHERE id= :id";
-		    $stmt = $this->db->prepare($sql);
-		    $data = [
-		    	'id' => $this->_id
-			];
-		    $stmt->execute($data);
-		    return "Ok";
-		} catch (Exception $e) {
-		    die("Oh noes! There's an error in the query!".$e);
-		}
-    }
-
-    // put TODO
-    public function put() {
-		try {
-    		$sql = "UPDATE student SET name = :name, surname = :surname, sidi_Code = :sidiCode, tax_Code = :taxCode WHERE id = :id";
-		    $stmt = $this->db->prepare($sql);
-		    $data = [
-				'id' => $this->_id,
-			    'name' => $this->_name,
-			    'surname' => $this->_surname,
-			    'sidiCode' => $this->_sidiCode,
-			    'taxCode' => $this->_taxCode,
-			];
-		    $stmt->execute($data);
-		    return "Ok";
-		} catch (Exception $e) {
-		    die("Oh noes! There's an error in the query!".$e);
-		}
-    }
- 
-    // patch TODO
-    public function patch() {
-		try {
-			$campi="";
-			if(!is_null($this->_name))
-				$campi .= "name = :name,";
-
-			if(!is_null($this->_surname))
-				$campi .= "surname = :surname,";
-
-			if(!is_null($this->_sidiCode))
-				$campi .= "sidi_code = :sidiCode,";
-
-			if(!is_null($this->_taxCode))
-				$campi .= "tax_code = :taxCode,";
-
-			$campi = rtrim($campi,",");
-
-    		$sql = "UPDATE student SET ".$campi." WHERE id = :id";
-			$stmt = $this->db->prepare($sql);
-			
-		    $data = [
-				'id' => $this->_id,
-			];
-		if(!is_null($this->_name))
-			$data['name'] = $this->_name;
-
-		if(!is_null($this->_surname))
-			$data['surname'] = $this->_surname;
-
-		if(!is_null($this->_sidiCode))
-			$data['sidiCode'] = $this->_sidiCode;
-
-		if(!is_null($this->_taxCode))
-			$data['taxCode'] = $this->_taxCode;
-
-		$stmt->execute($data);
-		echo $sql;
-		echo var_dump($data);
-		    return "Ok";
-		} catch (Exception $e) {
-		    die("Oh noes! There's an error in the query!".$e);
-		}
-    }
+		
  
 }
 ?>
--- Backend/registrazione.php
+++ Backend/registrazione.php
@@ -1,32 +1,9 @@
 <?php
 $requestMethod = $_SERVER["REQUEST_METHOD"];
-include('./class/Student.php');
+include('./class/Utente.php');
 $utente = new Utente();
 switch($requestMethod) {
-    case 'GET'://ok
 
-    $pathArray = explode('/', $_SERVER['REQUEST_URI']);
-    if(isset($pathArray[3]))
-        $id = $pathArray[3];
-    else
-        $id = -1;
-	
-		if($id != -1) {
-			$student->_id = $id;
-			$data = $student->one();
-		} else {
-            $data = $student->list();          
-        }
-        
-		if(!empty($data)) {
-          $js_encode = json_encode(array('status'=>TRUE, 'studentInfo'=>$data), true);
-        } else {
-          $js_encode = json_encode(array('status'=>FALSE, 'message'=>'There is no record yet.'), true);
-        }
-        header('Content-Type: application/json');
-		echo $js_encode;
-		break;
-    
     case 'POST'://Ok
         $inputJSON = file_get_contents('php://input');
         $input = json_decode($inputJSON, TRUE);
@@ -35,10 +12,8 @@ switch($requestMethod) {
         $utente->_cognome = $input["cognome"];
         $utente->_email = $input["email"];
         $utente->_password = $input["password"];
-        $utente->_sesso = $input["sesso"];
 
-
-        $data = $student->insert();
+        $student->insert();
         $js_encode = json_encode(array('status'=>TRUE, 'studentInfo'=>$data), true);
 
         header('Content-Type: application/json');
