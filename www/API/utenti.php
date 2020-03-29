974d70c67380ecbbc1ef0e5387d03397b54fbb3b
Petrucci <a.petru01@hotmail.com>
2020-03-29 22:56:05 +0200

    lista utenti iscritti

---
+++ www/API/utenti.php
@@ -0,0 +1,32 @@
+<?php
+include_once("class/Utente.php");
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+$utente = new Utente();
+
+switch($requestMethod)
+{
+
+    case 'GET'://Ok
+        session_start();
+
+        $utente->_id = $_SESSION["id"];
+
+        $profileInfo = $utente->get();
+
+        if($profileInfo[0]["authLevel"]>0){
+            $profileInfo2 = $utente->list();
+            echo json_encode($profileInfo2);
+        }
+        else
+	        header("HTTP/1.0 401 Unauthorized");
+        
+        break;
+    
+    default:
+	    header("HTTP/1.0 405 Method Not Allowed");
+	    break;
+}
+
+?>
